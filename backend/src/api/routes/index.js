c61dcaafc102d8b81c358a97dd4cee0c47f7bb2f
KHOA <khoa47245@gmail.com>
2024-11-28 11:13:06 +0700

    resolve conflic in routes/index.js

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -4,10 +4,7 @@ const routes = express.Router();
 routes.use('/students',require('./student.router'))
 routes.use('/printers',require('./printer.router'))
 routes.use('/uploads',require('./upload.router'))
-<<<<<<< HEAD
-=======
 routes.use('/logs',require('./log.router'))
 routes.use('/admins',require('./admin.router'))
 routes.use('/login',require('./login.router'))
->>>>>>> dev
 module.exports = routes;
